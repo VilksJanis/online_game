ac623d68131516a63bdc90f455bb14aabe76e8f4
jan <vilks.jan@gmail.com>
2021-04-19 21:10:09 +0200

    added blank game view

--- app/index.js
+++ app/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
