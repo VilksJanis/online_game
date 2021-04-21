d20d63ec21a25add17c4126f34a37f09f26c8168
jan <vilks.jan@gmail.com>
2021-04-22 00:11:02 +0200

    added basic websocket support

--- app/index.js
+++ app/index.js
@@ -1,3 +1,16 @@
+const ACTION_REGISTER = 'register';
+const ACTION_UPDATE_STATE = 'update';
+const ACTION_EDIT_SETTINGS = 'settings';
+
+
+const ws = require('ws');
+const uuid = require('uuid');
+
+
+
+const WS = new ws.Server({ port: 8082 });
+
+
 const express = require('express')
 const app = express();
 const port = 8080;
@@ -11,3 +24,33 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
   console.log(`Node.js backend running on: http://localhost:${port} !`)
 });
+
+
+WS.on('connection', socket => {
+  socket.uuid = uuid.v4().replaceAll("-", "").slice(0, 16);
+
+  socket.send(JSON.stringify({
+    code: ACTION_REGISTER,
+    payload: {
+      uuid: socket.uuid
+    }
+  }));
+
+
+  // process messages
+  socket.on('message', message => {
+    let [x, y, angle, clicked] = message.split(",");
+    console.log(socket.uuid, x, y, angle, clicked);
+  });
+
+
+  socket.on('close', () => {
+    console.log('ws closed')
+  });
+});
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
+// https://github.com/websockets/ws
+// https://github.com/engineer-man/math-arena/blob/master/gateway/gateway.js
+// https://github.com/B3L7/phaser3-tilemap-pack
--- app/public/js/game.js
+++ app/public/js/game.js
@@ -4,9 +4,9 @@ const playerSprite = 'assets/sprites/player.png';
 const box = 'assets/images/blue_square_50_50.png';
 
 const playerMoveSpeedAx = 200;
-const playerMoveSpeedDiagonal = (1.414 * playerMoveSpeedAx) / 2 ;
+const playerMoveSpeedDiagonal = (1.414 * playerMoveSpeedAx) / 2;
 function reportWindowSize() {
-  heightOutput.textContent =document.getElementById('div_game');
+  heightOutput.textContent = document.getElementById('div_game');
   widthOutput.textContent = window.innerWidth;
 }
 
@@ -28,7 +28,7 @@ var config = {
       debug: false,
       gravity: { y: 0 }
     }
-  },   
+  },
   width: 1024,
   height: 768,
   scene: {
@@ -47,10 +47,13 @@ function init() {
   var canvas = this.sys.game.canvas;
   var fullscreen = this.sys.game.device.fullscreen;
 
+  state_emit_timer = 0;
+  last_player_angle = 0;
+
   if (!fullscreen.available) {
     return;
   }
-  
+
   startBtn = document.getElementById('fullscreen');
 
   startBtn.addEventListener('click', function () {
@@ -75,52 +78,63 @@ function create() {
 
 
   this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
   });
 
   this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'player', frame: 4 } ],
-      frameRate: 20
+    key: 'turn',
+    frames: [{ key: 'player', frame: 4 }],
+    frameRate: 20
   });
 
   this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 7 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 7 }),
+    frameRate: 10,
+    repeat: -1
   });
 
   arrows = this.input.keyboard.createCursorKeys();
-  wasd = this.input.keyboard.addKeys({ 'W': Phaser.Input.Keyboard.KeyCodes.W, 'S': Phaser.Input.Keyboard.KeyCodes.S, 'A': Phaser.Input.Keyboard.KeyCodes.A, 'D': Phaser.Input.Keyboard.KeyCodes.D});
-  cursors = {...arrows, ...wasd};
+  wasd = this.input.keyboard.addKeys({ 'W': Phaser.Input.Keyboard.KeyCodes.W, 'S': Phaser.Input.Keyboard.KeyCodes.S, 'A': Phaser.Input.Keyboard.KeyCodes.A, 'D': Phaser.Input.Keyboard.KeyCodes.D });
+  cursors = { ...arrows, ...wasd };
 
-  cursors.check_pressed_up = function () {return this.up.isDown || this.W.isDown};
-  cursors.check_pressed_down = function () {return this.down.isDown || this.S.isDown};
-  cursors.check_pressed_left = function () {return this.left.isDown || this.A.isDown};
-  cursors.check_pressed_right = function () {return this.right.isDown || this.D.isDown};
+  cursors.check_pressed_up = function () { return this.up.isDown || this.W.isDown };
+  cursors.check_pressed_down = function () { return this.down.isDown || this.S.isDown };
+  cursors.check_pressed_left = function () { return this.left.isDown || this.A.isDown };
+  cursors.check_pressed_right = function () { return this.right.isDown || this.D.isDown };
 
+  this.input.on('pointermove', function (pointer) {
+    let angle = Phaser.Math.Angle.Between(player.x, player.y, pointer.x + this.cameras.main.scrollX, pointer.y + this.cameras.main.scrollY);
+    player.angle = angle;
+  }, this);
+
+  this.input.on("pointerdown", function (pointer){
+    send_movement_state(
+      player.x,
+      player.y,
+      Phaser.Math.Angle.Between(player.x, player.y, pointer.x + this.cameras.main.scrollX, pointer.y + this.cameras.main.scrollY),
+      1,
+      )
+  }, this);
 
   this.cameras.main.startFollow(player);
-  console.log(player)
-}   
+}
 
 
-
-function update() {
+function update(time, delta) {
+  var activity_detected = false;
   var xMovement = 0;
   var yMovement = 0;
   xMovement = cursors.check_pressed_left() ? -1 : cursors.check_pressed_right() ? 1 : 0;
   yMovement = cursors.check_pressed_up() ? -1 : cursors.check_pressed_down() ? 1 : 0;
-  
+
   if (cursors.shift.isDown) {
     xMovement = xMovement * 2;
     yMovement = yMovement * 2;
   }
-
 
   if (xMovement != 0 && yMovement != 0) {
     player.setVelocityX(xMovement * playerMoveSpeedDiagonal);
@@ -130,14 +144,33 @@ function update() {
     player.setVelocityX(xMovement * playerMoveSpeedAx);
     player.setVelocityY(yMovement * playerMoveSpeedAx);
     player.anims.play('right', true);
-
   }
 
-  
-}
+  activity_detected = xMovement + yMovement != 0 ? true : last_player_angle != player.angle ? true : false;
+  state_emit_timer += delta;
 
+  if (state_emit_timer >= 1000 || activity_detected) {
+    send_movement_state(
+      player.x,
+      player.y,
+      player.angle,
+      0
+    );
+    state_emit_timer = 0;
+    last_player_angle = player.angle;
+  }
+}
 
 
 function render() {
   this.debug.spriteInfo(s, 20, 32);
+}
+
+
+function send_movement_state(x,y,angle,clicked) {
+  webSocket.send([Math.round(x),Math.round(y),angle.toFixed(4), clicked].join());
+}
+
+function send_action(x,y,angle,action) {
+  console.log(x,y,angle,action);
 }
