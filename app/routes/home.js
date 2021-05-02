b174eb093534ca5f855e0a9b7497cca935071eaa
Jan Vilks <janis@shippingtechnology.com>
2021-05-02 16:57:32 +0200

    added landing page

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require('express');
+var path = require('path');
+
 let route_home = express.Router()
 
 route_home.get('/', function (req, res) {
     res.sendFile(path.join(__basedir, 'views/home.html'));
 });
-
 
 
 module.exports = route_home
